51af6f578a69ed89e3841d12a4c45f65b9256f77
Hector Medeiros <hector.medeiros@ccc.ufcg.edu.br>
2018-03-01 22:58:26 -0300

    Adicionando novas consultas

--- pt3/consultas_grupo07.sql
+++ pt3/consultas_grupo07.sql
@@ -10,6 +10,18 @@ ORDER BY VOLUMEMAXIMO, COMPRIMENTO DESC;
 
 SELECT MIN(PERIMETRO) AS MenorPerimetro FROM Bacia;
 
+/* 5. Qual o nome do usuário que mais realizou medições puviométricas, e quantas foram?
+    FALTA CONCLUIR (RESTRINGIR PARA UM UNICO USUARIO)*/
+SELECT u.nome, COUNT(*)
+	FROM usuario u, MedicaoPluviometrica m
+	WHERE (m.matricula = u.matricula) 
+	GROUP BY u.nome
+
+/*9. Liste a quantidade de postos pluviométricos por bacia. */
+SELECT b.nome, COUNT(*)
+	FROM PostoPluviometrico p, Bacia b
+	WHERE (p.idBacia = b.idBacia)
+	GROUP BY b.nome 
 
 /*11. Liste o nome dos postos pluviométricos localizados no Ceará ou em Alagoas.*/
 
